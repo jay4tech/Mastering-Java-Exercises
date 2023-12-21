609651c4d29201f5c8441bd3a521cc872c70b806
Jay Prakash Kumar <jayprakashatcgc@gmail.com>
2023-12-22 01:24:35 +0530

    Interface

---
+++ src/com/jay4tech/Interface/ISaveable.java
@@ -0,0 +1,11 @@
+package com.jay4tech.Interface;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public interface ISaveable {
+    // write code here
+    List<String> write();
+    
+    void read(List<String> lst);
+}
