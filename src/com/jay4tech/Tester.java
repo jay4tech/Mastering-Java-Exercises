341f4d00b619efd0b6ac0dbb1174a2bbe02df94b
Jay Prakash Kumar <jayprakashatcgc@gmail.com>
2023-12-21 20:09:30 +0530

    Initial Commit

---
+++ src/com/jay4tech/Tester.java
@@ -0,0 +1,9 @@
+package com.jay4tech;
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        PositiveNegativeZero.checkNumber(-10);
+    }
+}
