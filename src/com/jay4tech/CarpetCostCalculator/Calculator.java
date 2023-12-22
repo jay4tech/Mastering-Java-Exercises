c350caf7180809f116d55497a903817f24381620
Jay Prakash Kumar <jayprakashatcgc@gmail.com>
2023-12-22 11:49:29 +0530

    Carpet Cost Calculator
    Carpet Cost Calculator

---
+++ src/com/jay4tech/CarpetCostCalculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.jay4tech.CarpetCostCalculator;
+
+public class Calculator {
+    // write your code here
+    private Floor floor;
+    private Carpet carpet;
+    
+    public Calculator(Floor floor, Carpet carpet){
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+    
+    public double getTotalCost(){
+        return (floor.getArea() * carpet.getCost());
+    }
+}
